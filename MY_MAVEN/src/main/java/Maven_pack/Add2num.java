20f19719eae7de168ab6e2a5c6c66ac9f270c578
midhu <midhu@DESKTOP-FF9S3V8>
2024-03-28 01:23:30 -0400

    MY_Commit

---
+++ MY_MAVEN/src/main/java/Maven_pack/Add2num.java
@@ -0,0 +1,14 @@
+package Maven_pack;
+
+public class Add2num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=3;
+		int c = a+b;
+		System.out.println("value is : "+c);
+
+	}
+
+}
---
+++ MY_MAVEN/src/main/java/Maven_pack/Display.java
@@ -0,0 +1,10 @@
+package Maven_pack;
+
+public class Display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
